ffda31f7d84379c19dce98475d0bdbc085d0d206
Casey Brooks <cjbrooks12@gmail.com>
2016-12-16 12:00:41 -0600

    Fixes JS navigation issue

--- src/main/resources/assets/js/docs.js
+++ src/main/resources/assets/js/docs.js
@@ -1,8 +1,10 @@
 $(function(){
   // build side menu
+
+    console.log("init");
   var html = '';
 
-  $('.bs-orchid-section').each(function() {
+  $('.bs-docs-section').each(function() {
     var h1 = $(this).find('h1[id]').first(),
         h23 = $(this).find('h2[id], h3[id]');
 
@@ -26,7 +28,7 @@ $(function(){
     $('[role=main]').toggleClass('col-md-9 col-md-12');
   }
   else {
-    $('.bs-orchid-sidenav').html(html);
+    $('.bs-docs-sidenav').html(html);
   }
 
   $('[data-bootbox]').on('click', function() {
@@ -38,12 +40,12 @@ $(function(){
   });
 
   /*!
-   * JavaScript for Bootstrap's orchid (http://getbootstrap.com)
+   * JavaScript for Bootstrap's docs (http://getbootstrap.com)
    * Copyright 2011-2014 Twitter, Inc.
    * Licensed under the Creative Commons Attribution 3.0 Unported License. For
    * details, see http://creativecommons.org/licenses/by/3.0/.
    */
-  !function(a){a(function(){if(navigator.userAgent.match(/IEMobile\/10\.0/)){var b=document.createElement("style");b.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.querySelector("head").appendChild(b)}{var c=a(window),d=a(document.body);a(".navbar").outerHeight(!0)+10}d.scrollspy({target:".bs-orchid-sidebar"}),c.on("load",function(){d.scrollspy("refresh")}),a(".bs-orchid-container [href=#]").click(function(a){a.preventDefault()}),setTimeout(function(){var b=a(".bs-orchid-sidebar");b.affix({offset:{top:function(){var c=b.offset().top,d=parseInt(b.children(0).css("margin-top"),10),e=a(".bs-orchid-nav").height();return this.top=c-e-d},bottom:function(){return this.bottom=a(".bs-orchid-footer").outerHeight(!0)}}})},100),setTimeout(function(){a(".bs-top").affix()},100)})}(jQuery);
+  !function(a){a(function(){if(navigator.userAgent.match(/IEMobile\/10\.0/)){var b=document.createElement("style");b.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.querySelector("head").appendChild(b)}{var c=a(window),d=a(document.body);a(".navbar").outerHeight(!0)+10}d.scrollspy({target:".bs-docs-sidebar"}),c.on("load",function(){d.scrollspy("refresh")}),a(".bs-docs-container [href=#]").click(function(a){a.preventDefault()}),setTimeout(function(){var b=a(".bs-docs-sidebar");b.affix({offset:{top:function(){var c=b.offset().top,d=parseInt(b.children(0).css("margin-top"),10),e=a(".bs-docs-nav").height();return this.top=c-e-d},bottom:function(){return this.bottom=a(".bs-docs-footer").outerHeight(!0)}}})},100),setTimeout(function(){a(".bs-top").affix()},100)})}(jQuery);
 });
 
 function trianglify(color1, color2) {
