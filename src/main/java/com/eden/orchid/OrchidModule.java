c387c84c815a1f87a673cbf254df52b5960585d7
Casey Brooks <cjbrooks12@gmail.com>
2017-02-10 11:30:06 -0600

    Improves Module registration

--- src/main/java/com/eden/orchid/OrchidModule.java
+++ src/main/java/com/eden/orchid/OrchidModule.java
@@ -1,11 +1,33 @@
 package com.eden.orchid;
 
+import com.eden.orchid.api.resources.OrchidResourceSource;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 
+import javax.inject.Singleton;
+
 public abstract class OrchidModule extends AbstractModule {
-    protected <T> void addToSet(Class<T> setClass, Class<? extends T> objectClass) {
+
+    @SafeVarargs
+    protected final <T> void addToSet(Class<T> setClass, Class<? extends T>... objectClasses) {
         Multibinder<T> binder = Multibinder.newSetBinder(binder(), setClass);
-        binder.addBinding().to(objectClass);
+
+        if(objectClasses != null && objectClasses.length > 0) {
+            for(Class<? extends T> objectClass : objectClasses) {
+                if(setClass.isAssignableFrom(objectClass)) {
+                    binder.addBinding().to(objectClass);
+                }
+            }
+        }
+    }
+
+    protected final void addTheme(Class<? extends Theme> themeClass) {
+        bind(themeClass).in(Singleton.class);
+
+        Multibinder<Theme> themeBinder = Multibinder.newSetBinder(binder(), Theme.class);
+        themeBinder.addBinding().to(themeClass);
+
+        Multibinder<OrchidResourceSource> resourceSourceBinder = Multibinder.newSetBinder(binder(), OrchidResourceSource.class);
+        resourceSourceBinder.addBinding().to(themeClass);
     }
 }
--- src/main/java/com/eden/orchid/impl/compilers/jtwig/JTwigModule.java
+++ src/main/java/com/eden/orchid/impl/compilers/jtwig/JTwigModule.java
@@ -8,13 +8,15 @@ public class JTwigModule extends OrchidModule {
 
     @Override
     protected void configure() {
-        addToSet(JtwigFunction.class, TwigCompileAsFilter.class);
-        addToSet(JtwigFunction.class, TwigJsonFilter.class);
-        addToSet(JtwigFunction.class, TwigLinkFilter.class);
-        addToSet(JtwigFunction.class, TwigQueryFilter.class);
-        addToSet(JtwigFunction.class, TwigSortByFilter.class);
-        addToSet(JtwigFunction.class, TwigWalkMapFilter.class);
+        addToSet(JtwigFunction.class,
+                TwigCompileAsFilter.class,
+                TwigJsonFilter.class,
+                TwigLinkFilter.class,
+                TwigQueryFilter.class,
+                TwigSortByFilter.class,
+                TwigWalkMapFilter.class);
 
-        addToSet(TypedResourceLoader.class, OrchidResourceLoader.class);
+        addToSet(TypedResourceLoader.class,
+                OrchidResourceLoader.class);
     }
 }
