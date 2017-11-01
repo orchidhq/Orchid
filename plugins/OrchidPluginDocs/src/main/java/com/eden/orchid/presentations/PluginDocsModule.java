89e82f07c5bb54ff803c7ab8dffc023a1269cfa6
Casey Brooks <cjbrooks12@gmail.com>
2017-10-31 20:34:05 -0500

    Adds shim for PluginDocs to auto-document Orchid plugins, tweaks FutureImperfect theme

---
+++ plugins/OrchidPluginDocs/src/main/java/com/eden/orchid/presentations/PluginDocsModule.java
@@ -0,0 +1,12 @@
+package com.eden.orchid.presentations;
+
+import com.eden.orchid.OrchidModule;
+
+public class PluginDocsModule extends OrchidModule {
+
+    @Override
+    protected void configure() {
+
+    }
+
+}
