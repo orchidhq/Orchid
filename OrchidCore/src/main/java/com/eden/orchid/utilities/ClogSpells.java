4ef693d8a39925bcd4be2cd4437865151ddcb01b
Casey Brooks <cjbrooks12@gmail.com>
2017-12-01 10:36:26 -0600

    Adds several more helper Clog spells for getting parts of base URL

--- OrchidCore/src/main/java/com/eden/orchid/utilities/ClogSpells.java
+++ OrchidCore/src/main/java/com/eden/orchid/utilities/ClogSpells.java
@@ -3,11 +3,44 @@ package com.eden.orchid.utilities;
 import com.caseyjbrooks.clog.parseltongue.Spell;
 import com.eden.orchid.Orchid;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ClogSpells {
 
     @Spell
     public static String baseUrl(Object object) {
         return Orchid.getInstance().getContext().getSite().getBaseUrl();
+    }
+
+    @Spell
+    public static String baseUrlScheme(Object object) throws MalformedURLException {
+        return new URL(ClogSpells.baseUrl(object)).getProtocol();
+    }
+
+    @Spell
+    public static String baseUrlHost(Object object) throws MalformedURLException {
+        return new URL(ClogSpells.baseUrl(object)).getHost();
+    }
+
+    @Spell
+    public static int baseUrlPort(Object object) throws MalformedURLException {
+        return new URL(ClogSpells.baseUrl(object)).getPort();
+    }
+
+    @Spell
+    public static String baseUrlRoot(Object object) throws MalformedURLException {
+        URL url = new URL(ClogSpells.baseUrl(object));
+
+        String urlRoot = "";
+        urlRoot += url.getProtocol() + "://";
+        urlRoot += url.getHost();
+
+        if(url.getPort() != -1 && url.getPort() != 80) {
+            urlRoot += ":" + url.getPort();
+        }
+
+        return urlRoot;
     }
 
     @Spell
