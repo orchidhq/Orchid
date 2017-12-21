5b5ed8919ea8f795b4b496e589361ed0e45f7264
Casey Brooks <cjbrooks12@gmail.com>
2017-12-20 20:28:14 -0600

    Converts Wiki plugin to Kotlin

---
+++ plugins/OrchidWiki/src/main/kotlin/com/eden/orchid/wiki/model/WikiModel.kt
@@ -0,0 +1,35 @@
+package com.eden.orchid.wiki.model
+
+import com.eden.orchid.api.theme.pages.OrchidPage
+import java.util.*
+import javax.inject.Singleton
+
+@Singleton
+class WikiModel {
+
+    var sections: MutableMap<String?, WikiSection> = LinkedHashMap()
+        private set
+
+    val allPages: List<OrchidPage>
+        get() {
+            val allPages = ArrayList<OrchidPage>()
+            for (value in sections.values) {
+                allPages.add(value.summaryPage)
+                allPages.addAll(value.wikiPages)
+            }
+
+            return allPages
+        }
+
+    fun initialize() {
+        this.sections = LinkedHashMap()
+    }
+
+    fun getSection(sectionKey: String?) : WikiSection? {
+        if(sectionKey != null) {
+            return sections.getOrDefault(sectionKey, null)
+        }
+
+        return null
+    }
+}
