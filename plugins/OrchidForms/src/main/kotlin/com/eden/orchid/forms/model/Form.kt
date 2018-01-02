19122d22465702b7a21f262eaa31d102c7f9582c
Casey Brooks <cjbrooks12@gmail.com>
2018-01-01 23:51:43 -0600

    Adds descriptions to all Options in OrchidForms

--- plugins/OrchidForms/src/main/kotlin/com/eden/orchid/forms/model/Form.kt
+++ plugins/OrchidForms/src/main/kotlin/com/eden/orchid/forms/model/Form.kt
@@ -2,6 +2,7 @@ package com.eden.orchid.forms.model
 
 import com.eden.orchid.api.OrchidContext
 import com.eden.orchid.api.options.OptionsHolder
+import com.eden.orchid.api.options.annotations.Description
 import com.eden.orchid.api.options.annotations.Option
 import com.eden.orchid.api.options.annotations.StringDefault
 import org.json.JSONObject
@@ -10,15 +11,21 @@ import org.json.JSONObject
 class Form(protected val context: OrchidContext, val key: String, val formData: JSONObject) : OptionsHolder {
 
     @Option
+    @Description("The user-facing title of the form.")
     lateinit var title: String
 
     @Option
+    @Description("The 'action' attribute of the form. On forms which have a dedicated submission page, if this is " +
+            "not provided, it defaults to the URL of the submission page."
+    )
     lateinit var action: String
 
     @Option @StringDefault("POST")
+    @Description("The HTTP request method the form submits as. One of ['GET', 'POST'].")
     lateinit var method: String
 
     @Option
+    @Description("A map of arbitrary attributes to add to the form element.")
     lateinit var attributesMap: JSONObject
 
     var fields: MutableMap<String, FormField> = LinkedHashMap()
--- plugins/OrchidForms/src/main/kotlin/com/eden/orchid/forms/model/fields/HiddenField.kt
+++ plugins/OrchidForms/src/main/kotlin/com/eden/orchid/forms/model/fields/HiddenField.kt
@@ -1,6 +1,7 @@
 package com.eden.orchid.forms.model.fields
 
 import com.eden.orchid.api.OrchidContext
+import com.eden.orchid.api.options.annotations.Description
 import com.eden.orchid.api.options.annotations.Option
 import com.eden.orchid.forms.model.FormField
 import javax.inject.Inject
@@ -9,6 +10,7 @@ class HiddenField @Inject
 constructor(context: OrchidContext) : FormField(context) {
 
     @Option
+    @Description("The value of this hidden field.")
     lateinit var value: String
 
     override fun getTemplates(): List<String> {
