ee46014b8be6808608919a732e9d0f7415ade152
Casey Brooks <cjbrooks12@gmail.com>
2018-02-16 19:02:05 -0600

    Autogenerates AdminLists for all sets bound with OrchidModule addToSet

--- OrchidCore/src/main/java/com/eden/orchid/api/registration/OrchidModule.java
+++ OrchidCore/src/main/java/com/eden/orchid/api/registration/OrchidModule.java
@@ -1,10 +1,15 @@
 package com.eden.orchid.api.registration;
 
+import com.eden.orchid.Orchid;
+import com.eden.orchid.api.server.admin.AdminList;
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import com.google.inject.multibindings.Multibinder;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Orchid is built on top of the Guice Dependency Injection framework by Google. This framework allows for runtime
@@ -22,6 +27,8 @@ import java.util.Arrays;
  */
 public abstract class OrchidModule extends AbstractModule {
 
+    private static final Set<Class<?>> knownSets = new HashSet<>();
+
     public static <T> AbstractModule of(final Class<T> injectedClass, final T value) {
         return new AbstractModule() {
             @Override
@@ -31,12 +38,34 @@ public abstract class OrchidModule extends AbstractModule {
         };
     }
 
+    private void bindKnownSet(final Class<?> setClass) {
+        if(!knownSets.contains(setClass)) {
+            Multibinder<AdminList> binder = Multibinder.newSetBinder(binder(), AdminList.class);
+            binder.addBinding().toInstance(new AdminList() {
+                @Override
+                public String getKey() {
+                    return setClass.getSimpleName();
+                }
+
+                @Override
+                public Collection getItems() {
+                    return Orchid.getInstance().getContext().resolveSet(setClass);
+                }
+            });
+
+            knownSets.add(setClass);
+        }
+    }
+
     protected final <T> void addToSet(Class<T> setClass) {
         Multibinder.newSetBinder(binder(), setClass);
     }
 
     @SafeVarargs
     protected final <T> void addToSet(Class<T> setClass, Class<? extends T>... objectClasses) {
+        if(objectClasses.length > 0) {
+            bindKnownSet(setClass);
+        }
         Multibinder<T> binder = Multibinder.newSetBinder(binder(), setClass);
 
         Arrays.stream(objectClasses).forEach(objectClass -> {
@@ -48,6 +77,9 @@ public abstract class OrchidModule extends AbstractModule {
 
     @SafeVarargs
     protected final <T> void addToSet(Class<T> setClass, T... objects) {
+        if(objects.length > 0) {
+            bindKnownSet(setClass);
+        }
         Multibinder<T> binder = Multibinder.newSetBinder(binder(), setClass);
 
         Arrays.stream(objects).forEach(object -> {
@@ -56,4 +88,5 @@ public abstract class OrchidModule extends AbstractModule {
             }
         });
     }
+
 }
--- plugins/OrchidPluginDocs/src/main/kotlin/com/eden/orchid/plugindocs/PluginDocsModule.kt
+++ plugins/OrchidPluginDocs/src/main/kotlin/com/eden/orchid/plugindocs/PluginDocsModule.kt
@@ -4,20 +4,10 @@ import com.eden.orchid.api.compilers.TemplateTag
 import com.eden.orchid.api.registration.OrchidModule
 import com.eden.orchid.api.resources.resourceSource.PluginResourceSource
 import com.eden.orchid.api.server.OrchidController
-import com.eden.orchid.api.server.admin.AdminList
 import com.eden.orchid.api.theme.AdminTheme
 import com.eden.orchid.api.theme.components.OrchidComponent
 import com.eden.orchid.plugindocs.components.PluginDocsComponent
 import com.eden.orchid.plugindocs.controllers.AdminController
-import com.eden.orchid.plugindocs.lists.CompilersList
-import com.eden.orchid.plugindocs.lists.ComponentsList
-import com.eden.orchid.plugindocs.lists.GeneratorsList
-import com.eden.orchid.plugindocs.lists.MenuItemsList
-import com.eden.orchid.plugindocs.lists.OptionsList
-import com.eden.orchid.plugindocs.lists.ParsersList
-import com.eden.orchid.plugindocs.lists.ResourceSourcesList
-import com.eden.orchid.plugindocs.lists.TasksList
-import com.eden.orchid.plugindocs.lists.ThemesList
 import com.eden.orchid.plugindocs.tags.PluginDocsTag
 
 class PluginDocsModule : OrchidModule() {
@@ -25,17 +15,6 @@ class PluginDocsModule : OrchidModule() {
     override fun configure() {
         addToSet(PluginResourceSource::class.java,
                 PluginDocsResourceSource::class.java)
-
-        addToSet(AdminList::class.java,
-                CompilersList::class.java,
-                ComponentsList::class.java,
-                GeneratorsList::class.java,
-                OptionsList::class.java,
-                ParsersList::class.java,
-                ResourceSourcesList::class.java,
-                TasksList::class.java,
-                ThemesList::class.java,
-                MenuItemsList::class.java)
 
         addToSet(OrchidComponent::class.java,
                 PluginDocsComponent::class.java)
