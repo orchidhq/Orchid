0dacce3fd1b4a352f97e98a5734c6d6826e2fb94
Casey Brooks <cjbrooks12@gmail.com>
2018-10-06 16:13:57 -0500

    Adds async and defer attributes to JsPage

--- OrchidCore/src/main/java/com/eden/orchid/api/theme/assets/JsPage.java
+++ OrchidCore/src/main/java/com/eden/orchid/api/theme/assets/JsPage.java
@@ -2,13 +2,26 @@ package com.eden.orchid.api.theme.assets;
 
 import com.eden.orchid.api.options.annotations.Archetype;
 import com.eden.orchid.api.options.annotations.Description;
+import com.eden.orchid.api.options.annotations.Option;
 import com.eden.orchid.api.options.archetypes.AssetMetadataArchetype;
 import com.eden.orchid.api.resources.resource.InlineResource;
 import com.eden.orchid.api.resources.resource.OrchidResource;
+import lombok.Getter;
+import lombok.Setter;
 
 @Archetype(value = AssetMetadataArchetype.class, key = "assetmeta")
 @Description(value = "A Javascript static asset.", name = "JS Asset")
 public final class JsPage extends AssetPage {
+
+    @Option
+    @Getter
+    @Setter
+    private boolean async;
+
+    @Option
+    @Getter
+    @Setter
+    private boolean defer;
 
     public JsPage(Object source, String sourceKey, OrchidResource resource, String key, String title) {
         super(source, sourceKey, resource, key, title);
@@ -19,7 +32,19 @@ public final class JsPage extends AssetPage {
             return "<script>\n" + resource.compileContent(this) + "\n</script>";
         }
         else {
-            return "<script src=\"" + this.getLink() + "\"></script>";
+            String tagString = "<script";
+
+            if(async) {
+                tagString += " async";
+            }
+            if(defer) {
+                tagString += " defer";
+            }
+
+            tagString += " src=\"" + this.getLink() + "\"";
+            tagString += "></script>";
+
+            return tagString;
         }
     }
 
