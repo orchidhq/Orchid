4e22a956071a6e9d7331d9b02c5c619a9383726e
Yaroslav Matveychuk <yma-extern@marketlogicsoftware.com>
2018-10-15 23:20:51 +0900

    fix codereview issues

--- plugins/OrchidChangelog/src/main/kotlin/com/eden/orchid/changelog/publication/GithubReleasesPublisher.kt
+++ plugins/OrchidChangelog/src/main/kotlin/com/eden/orchid/changelog/publication/GithubReleasesPublisher.kt
@@ -3,17 +3,15 @@ package com.eden.orchid.changelog.publication
 import com.caseyjbrooks.clog.Clog
 import com.eden.orchid.api.OrchidContext
 import com.eden.orchid.api.options.annotations.Description
+import com.eden.orchid.api.options.annotations.Option
 import com.eden.orchid.api.publication.OrchidPublisher
 import com.eden.orchid.changelog.model.ChangelogModel
+import com.google.inject.name.Named
 import okhttp3.MediaType
+import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
-import okhttp3.Response
-import okhttp3.OkHttpClient
 import org.json.JSONObject
-import java.text.SimpleDateFormat
-import java.time.Duration
-import java.time.Instant
 import javax.inject.Inject
 
 @Description("Create releases directly on Github.", name = "GithubReleases")
@@ -22,12 +20,11 @@ class GithubReleasesPublisher
 constructor(
         context: OrchidContext,
         val model: ChangelogModel,
-        val client: OkHttpClient
+        val client: OkHttpClient,
+        @Named("githubToken") val githubToken: String
 ) : OrchidPublisher(context, "githubReleases", 100) {
 
-    @Description("A GitHub Personal Access Token.")
-    lateinit var githubToken: String
-
+    @Option
     @Description("The repository to release to, as [username/repo].")
     lateinit var repo: String
 
@@ -61,28 +58,9 @@ constructor(
         Clog.d("Github POST: {}", url)
         try {
             val response = client.newCall(request).execute()
-            timeoutRateLimit(response)
-
             if (!response.isSuccessful) {
                 val bodyString = response.body()!!.string()
                 Clog.e("{}", bodyString)
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun timeoutRateLimit(response: Response) {
-        try {
-            val rlLimit = Integer.parseInt(response.header("X-RateLimit-Limit")!!)
-            val rlRemaining = Integer.parseInt(response.header("X-RateLimit-Remaining")!!)
-            val rlReset = SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z").parse(response.header("X-RateLimit-Reset")).toInstant()
-            val current = Instant.now()
-            val d = Duration.between(rlReset, current)
-
-            // if we are nearing the rate limit, pause down a bit until it resets
-            if (rlRemaining * 1.0 / rlLimit * 1.0 < 0.1) {
-                Thread.sleep(Math.abs(d.toMillis()))
             }
         } catch (e: Exception) {
             e.printStackTrace()
