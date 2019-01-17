68c493e3c9870f550048138096737d3fa4232a1d
Casey Brooks <cjbrooks12@gmail.com>
2019-01-17 11:16:20 -0600

    Compiles multiple plantuml groups in a template into multiple diagrams

--- OrchidCore/src/test/kotlin/com.eden.orchid.testhelpers.kt
+++ OrchidCore/src/test/kotlin/com.eden.orchid.testhelpers.kt
@@ -9,22 +9,22 @@ import org.jsoup.select.NodeFilter
 import strikt.api.Assertion
 
 fun Assertion.Builder<TestResults>.somethingRendered(): Assertion.Builder<TestResults> =
-        passesIf("at least one page was rendered") {
+    assertThat("at least one page was rendered") {
             it.renderingSuccess && it.renderedPageMap.isNotEmpty()
         }
 
 fun Assertion.Builder<TestResults>.nothingRendered(): Assertion.Builder<TestResults> =
-        passesIf("no pages were rendered") {
+    assertThat("no pages were rendered") {
             it.renderingSuccess && it.renderedPageMap.isEmpty()
         }
 
 fun Assertion.Builder<TestResults>.pageWasRendered(name: String): Assertion.Builder<TestRenderer.TestRenderedPage> =
-        passesIf("page was rendered at $name") {
+    assertThat("page was rendered at $name") {
             it.renderingSuccess && it.renderedPageMap[name] != null
         }.get { renderedPageMap[name]!! }
 
 fun Assertion.Builder<TestResults>.pageWasNotRendered(name: String): Assertion.Builder<TestResults> =
-        passesIf("page was not rendered at $name") {
+    assertThat("page was not rendered at $name") {
             it.renderingSuccess && it.renderedPageMap[name] == null
         }
 
@@ -67,9 +67,15 @@ fun Assertion.Builder<Document>.select(cssQuery: String) = get { select(cssQuery
 @JvmName("select_elements")
 fun Assertion.Builder<Elements>.select(cssQuery: String) = get { select(cssQuery) }
 
-fun Assertion.Builder<Elements>.matches() = passesIf("matches at least one node") { it.isNotEmpty() }
+fun Assertion.Builder<Elements>.matches() = assertThat("matches at least one node") { it.isNotEmpty() }
 
-fun Assertion.Builder<Elements>.doesNotMatch() = passesIf("matches no nodes") { it.isEmpty() }
+fun Assertion.Builder<Elements>.matchCountIs(count: Int) = apply {
+    assert("matches exactly $count nodes", count) {
+        if (it.size == count) pass() else fail(it.size)
+    }
+}
+
+fun Assertion.Builder<Elements>.doesNotMatch() = assertThat("matches no nodes") { it.isEmpty() }
 
 fun Assertion.Builder<Elements>.innerHtml() = get { html().trimLines() }
 
@@ -86,7 +92,7 @@ fun Assertion.Builder<Elements>.attr(attrName: String) = get {
         null
 }
 
-fun Assertion.Builder<Elements>.hasClass(className: String) = passesIf("has class '%s'") { it.hasClass(className) }
+fun Assertion.Builder<Elements>.hasClass(className: String) = assertThat("has class '%s'") { it.hasClass(className) }
 
 
 private fun String.trimLines() = this
--- languageExtensions/OrchidDiagrams/src/main/kotlin/com/eden/orchid/languages/diagrams/PlantUmlCompiler.kt
+++ languageExtensions/OrchidDiagrams/src/main/kotlin/com/eden/orchid/languages/diagrams/PlantUmlCompiler.kt
@@ -18,36 +18,7 @@ constructor() : OrchidCompiler(800) {
     private val tags = arrayOf("uml", "salt", "math", "latex", "gantt")
 
     override fun compile(extension: String, source: String, data: Map<String, Any>?): String {
-        var result = source
-        try {
-            try {
-                // ensure string is wrapped in @startuml...@enduml
-                result = result.trim { it <= ' ' }
-                result = wrapDiagram(result)
-
-                // compile string to SVG
-                val os = ByteArrayOutputStream(1024)
-
-                val fileFormat = FileFormatOption(FileFormat.SVG)
-                fileFormat.hideMetadata()
-
-                val reader = SourceStringReader(result, "UTF-8")
-                reader.outputImage(os, fileFormat)
-                os.close()
-
-                var s = String(os.toByteArray(), Charset.forName("UTF-8"))
-                s = s.replace("<\\?(.*)\\?>".toRegex(), "") // remove XML declaration
-
-                return s
-            } catch (e: IOException) {
-                e.printStackTrace()
-                return ""
-            }
-
-        } catch (e: Throwable) {
-            e.printStackTrace()
-            return ""
-        }
+        return compileMultipleDiagrams(source)
     }
 
     override fun getOutputExtension(): String {
@@ -61,24 +32,68 @@ constructor() : OrchidCompiler(800) {
     private fun wrapDiagram(input: String): String {
         var isWrapped = false
 
-        var source = input
+        val source = input.trim()
 
         for (tag in tags) {
-            if (source.startsWith("@start" + tag)) {
-                isWrapped = true
-                break
-            }
-            if (source.endsWith("@end" + tag)) {
+            if (source.contains("@start$tag") && source.contains("@end$tag")) {
                 isWrapped = true
                 break
             }
         }
 
-        if (!isWrapped) {
-            source = "@startuml\n" + source
-            source = source + "\n@enduml"
-        }
-
-        return source
+        return if (!isWrapped) "@startuml\n$source\n@enduml" else source
     }
+
+    private fun compileMultipleDiagrams(input: String): String {
+        var formattedInput = wrapDiagram(input.trim())
+
+        for(tag in tags) {
+            val templateResult = StringBuffer(formattedInput.length)
+
+            val matcher = "@start$tag(.+?)@end$tag"
+                .toRegex(setOf(RegexOption.DOT_MATCHES_ALL, RegexOption.MULTILINE, RegexOption.IGNORE_CASE))
+                .toPattern()
+                .matcher(formattedInput)
+            while(matcher.find()) {
+                val content = matcher.group(1)
+                val replacement = compileSingleDiagram(content, tag)
+                matcher.appendReplacement(templateResult, "")
+                templateResult.append(replacement)
+            }
+            matcher.appendTail(templateResult)
+
+            formattedInput = templateResult.toString()
+        }
+
+        return formattedInput
+    }
+
+    private fun compileSingleDiagram(input: String, tag: String): String {
+        val formattedInput = """
+            @start$tag
+            $${input.trim()}
+            @end$tag
+        """.trimIndent()
+
+        try {
+            // compile string to SVG
+            val os = ByteArrayOutputStream(1024)
+
+            val fileFormat = FileFormatOption(FileFormat.SVG)
+            fileFormat.hideMetadata()
+
+            val reader = SourceStringReader(formattedInput, "UTF-8")
+            reader.outputImage(os, fileFormat)
+            os.close()
+
+            var s = String(os.toByteArray(), Charset.forName("UTF-8"))
+            s = s.replace("<\\?(.*)\\?>".toRegex(), "") // remove XML declaration
+
+            return s
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return ""
+        }
+    }
+
 }
