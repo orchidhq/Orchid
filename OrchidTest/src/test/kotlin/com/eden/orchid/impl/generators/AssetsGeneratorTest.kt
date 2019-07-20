213497caa1f6e745a81472b5c6d553cc851ba765
Casey Brooks <cjbrooks12@gmail.com>
2019-07-20 13:32:19 -0500

    Adds tests for all core generators: assets, sitemap, external indices

---
+++ OrchidTest/src/test/kotlin/com/eden/orchid/impl/generators/AssetsGeneratorTest.kt
@@ -0,0 +1,56 @@
+package com.eden.orchid.impl.generators
+
+import com.eden.orchid.strikt.nothingElseRendered
+import com.eden.orchid.strikt.pageWasNotRendered
+import com.eden.orchid.strikt.pageWasRendered
+import com.eden.orchid.strikt.printResults
+import com.eden.orchid.testhelpers.OrchidIntegrationTest
+import com.eden.orchid.testhelpers.withGenerator
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+
+class AssetsGeneratorTest : OrchidIntegrationTest(withGenerator<AssetsGenerator>()) {
+
+    @BeforeEach
+    internal fun setUp() {
+        enableLogging()
+    }
+
+    @Test
+    @DisplayName("By default, assets are loaded from `assets/media`")
+    fun test01() {
+        resource("assets/media/photo-one.jpg")
+        resource("assets/images/photo-two.jpg")
+
+        expectThat(execute())
+            .printResults()
+            .pageWasRendered("/assets/media/photo-one.jpg")
+            .pageWasNotRendered("/assets/images/photo-two.jpg")
+            .nothingElseRendered()
+    }
+
+    @Test
+    @DisplayName("The assets directory can be changed")
+    fun test02() {
+        configObject("assets", """
+            {
+                "sourceDirs": [
+                    "assets/images"
+                ]
+            }
+        """.trimIndent())
+
+        resource("assets/media/photo-one.jpg")
+        resource("assets/images/photo-two.jpg")
+
+        expectThat(execute())
+            .printResults()
+            .pageWasNotRendered("/assets/media/photo-one.jpg")
+            .pageWasRendered("/assets/images/photo-two.jpg")
+            .nothingElseRendered()
+    }
+
+
+}
---
+++ OrchidTest/src/test/kotlin/com/eden/orchid/impl/generators/ExternalIndexGeneratorTest.kt
@@ -0,0 +1,83 @@
+package com.eden.orchid.impl.generators
+
+import com.eden.orchid.strikt.asHtml
+import com.eden.orchid.strikt.innerHtmlMatches
+import com.eden.orchid.strikt.nothingElseRendered
+import com.eden.orchid.strikt.pageWasRendered
+import com.eden.orchid.strikt.select
+import com.eden.orchid.testhelpers.OrchidIntegrationTest
+import com.eden.orchid.testhelpers.withGenerator
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+
+class ExternalIndexGeneratorTest : OrchidIntegrationTest(
+    withGenerator<HomepageGenerator>(),
+    withGenerator<ExternalIndexGenerator>()
+) {
+
+    @BeforeEach
+    internal fun setUp() {
+        enableLogging()
+    }
+
+    @Test
+    @DisplayName("The ExternalIndexGenerator loads indices from `external.externalIndices`")
+    fun test01() {
+        configObject("external", """
+            {
+                "externalIndices": [
+                    "https://copper-leaf.github.io/trellis/meta/all.index.json"
+                ]
+            }
+        """.trimIndent())
+
+        resource("homepage.peb", """
+            {{ link("Introspection") }}
+        """.trimIndent())
+
+        expectThat(execute())
+            .pageWasRendered("/index.html") {
+                get { content }
+                    .asHtml()
+                    .select("body") {
+                        innerHtmlMatches { +"https://copper-leaf.github.io/trellis/wiki/features/introspection" }
+                    }
+            }
+            .pageWasRendered("/favicon.ico")
+            .pageWasRendered("/404.html")
+            .nothingElseRendered()
+    }
+
+    @Test
+    @DisplayName("The ExternalIndexGenerator loads indices from `services.generators.externalIndices` for backward-compatibility")
+    fun test02() {
+        configObject("services", """
+            {
+                "generators": {
+                    "externalIndices": [
+                        "https://copper-leaf.github.io/trellis/meta/all.index.json"
+                    ]
+                }
+            }
+        """.trimIndent())
+
+        resource("homepage.peb", """
+            {{ link("Introspection") }}
+        """.trimIndent())
+
+        expectThat(execute())
+            .pageWasRendered("/index.html") {
+                get { content }
+                    .asHtml()
+                    .select("body") {
+                        innerHtmlMatches { +"https://copper-leaf.github.io/trellis/wiki/features/introspection" }
+                    }
+            }
+            .pageWasRendered("/favicon.ico")
+            .pageWasRendered("/404.html")
+            .nothingElseRendered()
+    }
+
+}
