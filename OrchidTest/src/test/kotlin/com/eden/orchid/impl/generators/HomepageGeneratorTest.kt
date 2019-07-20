726d299165951caa45d7cc2b73e7dfe9f9623cfa
Casey Brooks <cjbrooks12@gmail.com>
2019-07-20 12:10:44 -0500

    Updates homepage generator tests

--- OrchidTest/src/test/kotlin/com/eden/orchid/impl/generators/HomepageGeneratorTest.kt
+++ OrchidTest/src/test/kotlin/com/eden/orchid/impl/generators/HomepageGeneratorTest.kt
@@ -2,25 +2,24 @@ package com.eden.orchid.impl.generators
 
 import com.eden.orchid.strikt.asHtml
 import com.eden.orchid.strikt.innerHtmlMatches
+import com.eden.orchid.strikt.nothingElseRendered
 import com.eden.orchid.strikt.pageWasRendered
-import com.eden.orchid.strikt.printResults
 import com.eden.orchid.strikt.select
 import com.eden.orchid.testhelpers.OrchidIntegrationTest
 import com.eden.orchid.testhelpers.withGenerator
+import kotlinx.html.em
+import kotlinx.html.p
+import kotlinx.html.strong
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
-import strikt.assertions.isNull
-import strikt.assertions.isTrue
 
 class HomepageGeneratorTest : OrchidIntegrationTest(withGenerator<HomepageGenerator>()) {
 
     @Test
-    @DisplayName("Homepage generator runs fine")
+    @DisplayName("Homepage generator creates a homepage and 404 pages by default")
     fun test01() {
         expectThat(execute())
-            .and { get { isRenderingSuccess }.isTrue() }
-            .and { get { thrownException }.isNull() }
             .pageWasRendered("/index.html") {
                 get { content }
                     .asHtml()
@@ -28,36 +27,93 @@ class HomepageGeneratorTest : OrchidIntegrationTest(withGenerator<HomepageGenera
                         innerHtmlMatches { +"" }
                     }
             }
+            .pageWasRendered("/favicon.ico")
+            .pageWasRendered("/404.html")
+            .nothingElseRendered()
     }
 
     @Test
-    @DisplayName("Homepage generator runs fine")
+    @DisplayName("If a homepage.md file exists, it will be used")
     fun test02() {
+        resource("homepage.md", "**bold** _italic_")
+
         expectThat(execute())
-            .and { get { isRenderingSuccess }.isTrue() }
-            .and { get { thrownException }.isNull() }
             .pageWasRendered("/index.html") {
                 get { content }
                     .asHtml()
                     .select("body") {
-                        innerHtmlMatches { +"" }
+                        innerHtmlMatches {
+                            p {
+                                strong { +"bold" }
+                                em { +"italic" }
+                            }
+                        }
                     }
             }
+            .pageWasRendered("/favicon.ico")
+            .pageWasRendered("/404.html")
+            .nothingElseRendered()
     }
 
     @Test
-    @DisplayName("Homepage generator runs fine")
+    @DisplayName("The homepage file can be any valid file extension")
     fun test03() {
+        resource("homepage.peb", "**bold** _italic_")
+
         expectThat(execute())
-            .printResults()
-            .and { get { isRenderingSuccess }.isTrue() }
-            .and { get { thrownException }.isNull() }
             .pageWasRendered("/index.html") {
                 get { content }
                     .asHtml()
                     .select("body") {
-                        innerHtmlMatches { +"" }
+                        innerHtmlMatches { +"**bold** _italic_" }
                     }
             }
+            .pageWasRendered("/favicon.ico")
+            .pageWasRendered("/404.html")
+            .nothingElseRendered()
+    }
+
+
+
+
+    @Test
+    @DisplayName("If a 404.md file exists, it will be used")
+    fun test04() {
+        resource("404.md", "**bold** _italic_")
+
+        expectThat(execute())
+            .pageWasRendered("/index.html")
+            .pageWasRendered("/favicon.ico")
+            .pageWasRendered("/404.html") {
+                get { content }
+                    .asHtml()
+                    .select("body") {
+                        innerHtmlMatches {
+                            p {
+                                strong { +"bold" }
+                                em { +"italic" }
+                            }
+                        }
+                    }
+            }
+            .nothingElseRendered()
+    }
+
+    @Test
+    @DisplayName("The 404 file can be any valid file extension")
+    fun test05() {
+        resource("404.peb", "**bold** _italic_")
+
+        expectThat(execute())
+            .pageWasRendered("/index.html")
+            .pageWasRendered("/favicon.ico")
+            .pageWasRendered("/404.html") {
+                get { content }
+                    .asHtml()
+                    .select("body") {
+                        innerHtmlMatches { +"**bold** _italic_" }
+                    }
+            }
+            .nothingElseRendered()
     }
 }
