b821de95ddcc98ad3302cbda0adbb440b528f7e1
Casey Brooks <cjbrooks12@gmail.com>
2019-07-22 09:31:27 -0500

    Improves reliability of ScriptPublisherTest

--- OrchidTest/src/test/kotlin/com/eden/orchid/impl/publication/ScriptPublisherTest.kt
+++ OrchidTest/src/test/kotlin/com/eden/orchid/impl/publication/ScriptPublisherTest.kt
@@ -1,19 +1,48 @@
 package com.eden.orchid.impl.publication
 
 import com.caseyjbrooks.clog.Clog
+import com.caseyjbrooks.clog.ClogLogger
+import com.caseyjbrooks.clog.ClogProfile
 import com.eden.orchid.strikt.nothingElseRendered
 import com.eden.orchid.testhelpers.OrchidIntegrationTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
-import strikt.assertions.isEqualTo
+import strikt.assertions.isTrue
 
-class ScriptPublisherTest : OrchidIntegrationTest() {
+class ScriptPublisherTest : OrchidIntegrationTest(), ClogLogger {
+
+    var scriptPublisherLogged = false
+
+    @BeforeEach
+    internal fun setUp() {
+        scriptPublisherLogged = false
+
+        enableLogging()
+
+        Clog.setCurrentProfile("ScriptPublisherTest") {
+            ClogProfile().apply {
+                addLogger(null, this@ScriptPublisherTest)
+                addLogger(Clog.KEY_V, this@ScriptPublisherTest)
+                addLogger(Clog.KEY_D, this@ScriptPublisherTest)
+                addLogger(Clog.KEY_I, this@ScriptPublisherTest)
+                addLogger(Clog.KEY_W, this@ScriptPublisherTest)
+                addLogger(Clog.KEY_E, this@ScriptPublisherTest)
+                addLogger(Clog.KEY_WTF, this@ScriptPublisherTest)
+            }
+        }
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        disableLogging()
+        // restore current Clog profile
+        Clog.setCurrentProfile(null)
+    }
 
     @Test
     fun test01() {
-        enableLogging()
-        Clog.getInstance().addTagToWhitelist("Script Publisher")
-
         flag("task", "deploy")
         configObject(
             "services", """
@@ -36,10 +65,28 @@ class ScriptPublisherTest : OrchidIntegrationTest() {
         expectThat(execute())
             .nothingElseRendered()
 
-        expectThat(Clog.getInstance()) {
-            get { lastTag }.isEqualTo("Script Publisher")
-            get { lastLog }.isEqualTo("'script publisher message'")
-        }
+        expectThat(scriptPublisherLogged).isTrue()
     }
+
+
+// Clog Logger implementation
+//----------------------------------------------------------------------------------------------------------------------
+
+    override fun isActive(): Boolean = true
+
+    override fun priority(): Clog.Priority = Clog.Priority.VERBOSE
+
+    override fun log(tag: String?, message: String?): Int {
+        if(tag == "Script Publisher") {
+            scriptPublisherLogged = true
+        }
+
+        return 0
+    }
+
+    override fun log(tag: String?, message: String?, throwable: Throwable?): Int {
+        return log(tag, message)
+    }
+
 
 }
