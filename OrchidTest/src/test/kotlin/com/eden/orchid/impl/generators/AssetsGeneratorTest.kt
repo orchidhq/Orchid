c328cdcca4f9bdd2b433a36b644cc781104a9e94
Casey Brooks <cjbrooks12@gmail.com>
2019-08-06 17:12:43 -0500

    Fixes broken tests

--- OrchidTest/src/test/kotlin/com/eden/orchid/impl/generators/AssetsGeneratorTest.kt
+++ OrchidTest/src/test/kotlin/com/eden/orchid/impl/generators/AssetsGeneratorTest.kt
@@ -3,10 +3,8 @@ package com.eden.orchid.impl.generators
 import com.eden.orchid.strikt.nothingElseRendered
 import com.eden.orchid.strikt.pageWasNotRendered
 import com.eden.orchid.strikt.pageWasRendered
-import com.eden.orchid.strikt.printResults
 import com.eden.orchid.testhelpers.OrchidIntegrationTest
 import com.eden.orchid.testhelpers.withGenerator
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
@@ -20,7 +18,6 @@ class AssetsGeneratorTest : OrchidIntegrationTest(withGenerator<AssetsGenerator>
         resource("assets/images/photo-two.jpg")
 
         expectThat(execute())
-            .printResults()
             .pageWasRendered("/assets/media/photo-one.jpg")
             .pageWasNotRendered("/assets/images/photo-two.jpg")
             .nothingElseRendered()
@@ -29,19 +26,20 @@ class AssetsGeneratorTest : OrchidIntegrationTest(withGenerator<AssetsGenerator>
     @Test
     @DisplayName("The assets directory can be changed")
     fun test02() {
-        configObject("assets", """
+        configObject(
+            "assets", """
             {
                 "sourceDirs": [
                     "assets/images"
                 ]
             }
-        """.trimIndent())
+            """.trimIndent()
+        )
 
         resource("assets/media/photo-one.jpg")
         resource("assets/images/photo-two.jpg")
 
         expectThat(execute())
-            .printResults()
             .pageWasNotRendered("/assets/media/photo-one.jpg")
             .pageWasRendered("/assets/images/photo-two.jpg")
             .nothingElseRendered()
