76819123da6a68e883de9a20856cee2cd04f4062
Casey Brooks <cjbrooks12@gmail.com>
2019-09-08 15:07:14 -0500

    jmfayard/buildSrcVersions#61 Refactor to Enum and extension functions
    https://github.com/jmfayard/buildSrcVersions/issues/61
    
    small

--- bundles/OrchidAll/build.gradle.kts
+++ bundles/OrchidAll/build.gradle.kts
@@ -1,8 +1,7 @@
 dependencies {
-    compile(Projects.OrchidCore(this))
-
-    Projects.Plugins.all(this).forEach { compile(it) }
-    Projects.Themes.all(this).forEach { compile(it) }
-    Projects.LanguageExtensions.all(this).forEach { compile(it) }
-    Projects.Integrations.all(this).forEach { compile(it) }
+    implementation(Module.OrchidCore)
+    implementation(Projects.Plugins.all)
+    implementation(Projects.Themes.all)
+    implementation(Projects.LanguageExtensions.all)
+    implementation(Projects.Integrations.all)
 }
--- plugins/OrchidGroovydoc/build.gradle.kts
+++ plugins/OrchidGroovydoc/build.gradle.kts
@@ -2,5 +2,5 @@
 dependencies {
     implementation(Libs.com_eden_groovydoc_runner)
     implementation(Libs.com_eden_kodiak_groovydoc_runner)
-    implementation(Projects.Plugins.OrchidSourceDoc(this))
+    implementation(Module.OrchidSourceDoc)
 }
