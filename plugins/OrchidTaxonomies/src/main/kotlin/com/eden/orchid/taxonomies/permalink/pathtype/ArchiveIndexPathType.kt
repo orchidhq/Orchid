b96ba85f414fc863508d6aa7288db65709f93701
Casey Brooks <cjbrooks12@gmail.com>
2019-09-29 21:59:05 -0500

    Creates simple taxonomy archives from pre-existing collections

--- plugins/OrchidTaxonomies/src/main/kotlin/com/eden/orchid/taxonomies/permalink/pathtype/ArchiveIndexPathType.kt
+++ plugins/OrchidTaxonomies/src/main/kotlin/com/eden/orchid/taxonomies/permalink/pathtype/ArchiveIndexPathType.kt
@@ -2,6 +2,7 @@ package com.eden.orchid.taxonomies.permalink.pathtype
 
 import com.eden.orchid.api.theme.pages.OrchidPage
 import com.eden.orchid.api.theme.permalinks.PermalinkPathType
+import com.eden.orchid.taxonomies.pages.CollectionArchivePage
 import com.eden.orchid.taxonomies.pages.TaxonomyArchivePage
 import com.eden.orchid.taxonomies.pages.TermArchivePage
 import javax.inject.Inject
@@ -11,12 +12,14 @@ class ArchiveIndexPathType
 constructor() : PermalinkPathType(100) {
 
     override fun acceptsKey(page: OrchidPage, key: String): Boolean {
-        return key == "archiveIndex" && (page is TaxonomyArchivePage || page is TermArchivePage)
+        return key == "archiveIndex" &&
+                (page is TaxonomyArchivePage || page is TermArchivePage || page is CollectionArchivePage)
     }
 
     override fun format(page: OrchidPage, key: String): String? {
         val index = if (page is TaxonomyArchivePage) page.index
         else if (page is TermArchivePage) page.index
+        else if (page is CollectionArchivePage) page.index
         else 0
 
         return if (index == 0) null else if (index == 1) "" else "$index"
