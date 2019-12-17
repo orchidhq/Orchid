a919d5af196a0c77971c942a433f3c13eb0a196e
Casey Brooks <cjbrooks12@gmail.com>
2019-12-17 15:56:56 -0600

    Adds Mermaid JS component to OrchidDiagrams plugin. #330

--- languageExtensions/OrchidDiagrams/src/main/kotlin/com/eden/orchid/languages/diagrams/DiagramsModule.kt
+++ languageExtensions/OrchidDiagrams/src/main/kotlin/com/eden/orchid/languages/diagrams/DiagramsModule.kt
@@ -2,11 +2,15 @@ package com.eden.orchid.languages.diagrams
 
 import com.eden.orchid.api.compilers.OrchidCompiler
 import com.eden.orchid.api.registration.OrchidModule
+import com.eden.orchid.api.theme.components.OrchidComponent
+import com.eden.orchid.languages.diagrams.components.MermaidJsComponent
 import com.eden.orchid.utilities.addToSet
 
 class DiagramsModule : OrchidModule() {
 
     override fun configure() {
+        withResources()
         addToSet<OrchidCompiler, PlantUmlCompiler>()
+        addToSet<OrchidComponent, MermaidJsComponent>()
     }
 }
