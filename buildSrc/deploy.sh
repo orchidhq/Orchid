09991db19c5864401845c78a63b2f2d600030950
Casey Brooks <cjbrooks12@gmail.com>
2020-05-29 09:13:07 -0500

    More fixes to deploy script

--- buildSrc/deploy.sh
+++ buildSrc/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-./gradlew publishPlugins -Dgradle.publish.key=${GRADLE_PUBLISH_KEY} -Dgradle.publish.secret=${GRADLE_PUBLISH_SECRET}
+./gradlew publishPlugins -Dgradle.publish.key=${GRADLE_PUBLISH_KEY} -Dgradle.publish.secret=${GRADLE_PUBLISH_SECRET} -PorchidEnvironment=prod -Prelease
